aa994c4bf92f1e62ac761c812837e619eb0ea454
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-05-03 12:08:27 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
-owner_id = 1243703097
-bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-bot_id = 1663901971
+owner_id =  1221558981
+bot_token = "1641954476:AAG-EBjPfq8mj7OI6odusgtAa13j9xCQyOo"
+bot_id = 1641954476
 
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
